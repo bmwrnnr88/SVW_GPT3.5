59dbf746aaef112514612fe0f1efdbe1d60ebca8
bmwrnnr88 <138243329+bmwrnnr88@users.noreply.github.com>
2023-12-05 15:27:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -32,39 +32,7 @@ st.sidebar.title("Title")
 st.sidebar.divider()
 st.sidebar.markdown("Your name", unsafe_allow_html=True)
 st.sidebar.markdown("Assistant GPT")
-st.sidebar.divider()
-
-###
-# File uploader for CSV, XLS, XLSX
-uploaded_file = st.file_uploader("Upload your file", type=["csv", "xls", "xlsx"])
-
-if uploaded_file is not None:
-    # Determine the file type
-    file_type = uploaded_file.type
-
-    try:
-        # Read the file into a Pandas DataFrame
-        if file_type == "text/csv":
-            df = pd.read_csv(uploaded_file)
-        elif file_type in ["application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]:
-            df = pd.read_excel(uploaded_file)
-
-        # Convert DataFrame to JSON
-        json_str = df.to_json(orient='records', indent=4)
-        file_stream = io.BytesIO(json_str.encode())
-
-        # Upload JSON data to OpenAI and store the file ID
-        file_response = client.files.create(file=file_stream, purpose='answers')
-        st.session_state.file_id = file_response.id
-        st.success("File uploaded successfully to OpenAI!")
-
-        # Optional: Display and Download JSON
-        st.text_area("JSON Output", json_str, height=300)
-        st.download_button(label="Download JSON", data=json_str, file_name="converted.json", mime="application/json")
-    
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-     ###
+st.sidebar.divider()  
 
 # Initialize OpenAI assistant
 if "assistant" not in st.session_state:

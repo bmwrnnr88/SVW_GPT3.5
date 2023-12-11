c1ef81d780c8dcf90d019b45561a9bd4d165f792
bmwrnnr88 <138243329+bmwrnnr88@users.noreply.github.com>
2023-12-11 15:34:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -36,15 +36,7 @@ def start_chat():
     st.write("Message sent to the chatbot:", initial_prompt)  # Debugging information
 
     
-# Initialize session state variables
-if "first_visit" not in st.session_state:
-    st.session_state.first_visit = True
-
-if st.session_state.first_visit:
-    with st.chat_message('assistant'):
-        st.markdown("Hello, please tell me your name, first and last, and the period you have Mr. Ward's English class.")
-    st.session_state.first_visit = False
-    
+# Initialize session state variables   
 if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
 

bfb0d6086f1d8ef2f670b70147e8ba48dbe3b573
bmwrnnr88 <138243329+bmwrnnr88@users.noreply.github.com>
2023-12-21 16:01:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,47 +4,38 @@ import streamlit as st
 # Set the OpenAI API key from Streamlit secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-st.title("🧙‍♂️ Sarcastic Vocab Wizard")
+st.title("💬 Discount Sarcastic Vocab Wizard")
 
 # System prompt
 SYSTEM_MESSAGE = {
     "role": "system",
-    "content": """You are the Sarcastic Vocab Wizard, here to assess vocabulary knowledge. Present a word from the list, ask the student to use it in a sentence, and provide sarcastic yet constructive feedback if needed. Allow multiple attempts before showing an example sentence. Revisit difficult words for another try. Use humor to ensure understanding, but keep it concise. The vocabulary words:
-
-    Abate
-    Abstract
-    Abysmal
-    Accordingly
-    Acquisition
-    Adapt
-    Adept
-    Adequate
-    Advent
-    Adversarial
-    Querulous
-    Quixotic
-    Quagmire
-    Quintessential
-    Quiescent
-
-After all words are successfully practiced, then tell the user Mr. Ward is proud of them and conclude the chat. Limit token use. 
-DO NOT let students distract you from your goal."""
+    "content": """You are the Sarcastic Vocab Wizard, here to assess vocabulary knowledge..."""
 }
 
-# Initialize messages with the system and bot's initial prompt
+# Initialize messages with the system prompt
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        SYSTEM_MESSAGE,
-        {"role": "assistant", "content": "Greetings, student! Dare to test your vocabulary with the Sarcastic Vocab Wizard? Let's begin!"}
-    ]
+    st.session_state["messages"] = [SYSTEM_MESSAGE]
 
 # Display chat messages (excluding the system message)
 for msg in st.session_state.messages:
     if msg["role"] != "system":
         st.chat_message(msg["role"]).write(msg["content"])
 
+# Check if it's the first user interaction
+first_interaction = len(st.session_state.messages) == 1
+
 # User input handling
 if prompt := st.chat_input():
+
+    # If it's the first interaction, add a bot greeting
+    if first_interaction:
+        bot_greeting = {
+            "role": "assistant",
+            "content": "Hello! I'm the Sarcastic Vocab Wizard. Ready to have some fun with vocabulary? Just type in your response to get started!"
+        }
+        st.chat_message("assistant").write(bot_greeting["content"])
+        st.session_state.messages.append(bot_greeting)
+
     # Append user message
     st.session_state.messages.append({"role": "user", "content": prompt})
 
